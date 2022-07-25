28cc25bb74bcdedcaf83cf7bc9e0c74e36efa94f
권민지 <ming___jee@naver.com>
2022-07-26 04:26:32 +0900

    refactoring:2022-07-26 난이도 중 / 2차원 배열, 회전, 출력문(sep,end) + 함수 정의

--- SWEA(SW Expert Academy)/D2_1961_숫자 배열 회전.py
+++ SWEA(SW Expert Academy)/D2_1961_숫자 배열 회전.py
@@ -2,28 +2,23 @@
 # author: psS2mj
 # brief: SWEA_1961_숫자 배열 회전 (D2)
 
+def rotate(arr, N):
+    new_arr = [[0] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            new_arr[j][N-1-i] = arr[i][j]
+    return new_arr
+
 T = int(input())
 for tc in range(1, T+1):
     N = int(input())
     arr = [0 for _ in range(N)]
     for i in range(N):
         arr[i] = list(map(int, input().split()))
-    rotate90 = [[0] * N for _ in range(N)]
-    rotate180 = [[0] * N for _ in range(N)]
-    rotate270 = [[0] * N for _ in range(N)]
 
-    # 90도 회전
-    for i in range(N):
-        for j in range(N):
-            rotate90[j][N-1-i] = arr[i][j]
-    # 180도 회전
-    for i in range(N):
-        for j in range(N):
-            rotate180[j][N-1-i] = rotate90[i][j]
-    # 270도 회전
-    for i in range(N):
-        for j in range(N):
-            rotate270[j][N-1-i] = rotate180[i][j]
+    rotate90 = rotate(arr, N)
+    rotate180 = rotate(rotate90, N)
+    rotate270 = rotate(rotate180, N)
 
     print("#{}".format(tc))
     for i in range(N):
