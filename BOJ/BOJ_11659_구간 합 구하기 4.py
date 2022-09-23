315a2c23ba23a9c68b4d98906965ab7895934a73
권민지 <ming___jee@naver.com>
2022-09-23 21:34:19 +0900

    2022-09-23 실버 3 / 구간합, 입력 속도

---
+++ BOJ/BOJ_11659_구간 합 구하기 4.py
@@ -0,0 +1,19 @@
+# date: 2022/09/23
+# author: psS2mj
+# brief: BOJ_11659_구간 합 구하기 4
+
+import sys
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+S = [0]
+temp = 0
+
+for i in arr:
+    temp = temp + i
+    S.append(temp)
+
+for i in range(M):
+    s, e = map(int, input().split())
+    print(S[e] - S[s-1])
